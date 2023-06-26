d9ee4b9e0133015dbe13123ed37bca3531a70de7
toriq <thoriqahmad17@gmail.com>
2023-06-26 14:47:03 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header, Services, About, Qualities, Features, Portfolio, Testimonials, Contact, Footer } from './components'; 
+import './assets/js/script'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Services />
+      <About />
+      <Qualities />
+      <Features />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
